7a3c9797b069124e930ecb32722abd4b234b2974
TANIGAWA Takashi <mihailjp@gmail.com>
2014-02-27 23:21:30 +0900

    Add data types

--- MahjongHS.hs
+++ MahjongHS.hs
@@ -1,1 +1,11 @@
-module MahjongHS () where
+module MahjongHS (..) where
+
+----------------------------------
+-- Data types for Mahjong tiles --
+----------------------------------
+data Wind = East | South | West | North
+data Dragon = Red | Green | Blue
+data Honor = Wnd Wind | Drg Dragon
+data Suit = Char Int | Bamb Int | Circ Int
+data Flower = Plum | Orchid | Chrys | Bamboo | Spring | Summer | Autumn | Winter
+data Tile = Hn Honor | St Suit | Fl Flower
